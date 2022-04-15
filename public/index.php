28f72c4ce4e40b36b45e366a9238487903eda4f3
Oleksandr Savenkov <savenkovalx@gmail.com>
2022-04-15 18:48:23 +0300

    Add Router

--- public/index.php
+++ public/index.php
@@ -2,7 +2,5 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
-
+require_once CONF . '/routes.php';
 new \ishop\App();
-
-throw new Exception('Site no reach', 500);
