1be4ba6089b3e96c1ffc10d8cf29667d37a6ee54
Oleksandr Savenkov <savenkovalx@gmail.com>
2022-04-16 19:13:23 +0300

    Add store main template

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,14 +9,7 @@ class MainController extends AppController
 {
     public function indexAction()
     {
-        $posts = R::findAll('test');
         $this->setMeta('Main page', 'description...', 'keys....');
-        $name = 'John';
-        $age = '30';
-        $cache = Cache::instance();
-//        $cache->set('test', $posts);
-        $cache->delete('test');
-        $this->set(compact('name', 'age', 'posts'));
     }
 
 }
